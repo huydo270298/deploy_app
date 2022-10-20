ad4c50fbbef8d832d31d91b0c3253ad624b04fd0
huydo270298 <80236153+huydo270298@users.noreply.github.com>
2022-10-21 00:26:35 +0700

    add func play video

--- src/pages/components/VideoSection/VideoSection.js
+++ src/pages/components/VideoSection/VideoSection.js
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 import ReactPlayer from 'react-player'
 
 import styles from './VideoSection.module.scss';
@@ -6,16 +7,29 @@ import styles from './VideoSection.module.scss';
 let cx = classNames.bind(styles);
 
 const VideoSection = () => {
-  
+  const [play, setPlay] = useState(false);
+  const handleClickPlay = () => {
+    setPlay(!play)
+  }
   return (
-    <ReactPlayer 
-      url='https://www.youtube.com/watch?v=DVVIKctYZ68' 
-      playing={true} 
-      volume={1}
-      width='100%'
-      height='auto'
-      className={cx('wrapper')}
-    />
+    <div className={cx('wrapper')}>
+      <ReactPlayer
+        url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+        playing={play}
+        controls={false}
+        volume={1}
+        width='100%'
+        height='auto'
+        pip={true}
+        className={cx('box')}
+      // onPlay={() => console.log('play')}
+      />
+      <div className={cx('group_btn')}>
+        <button className={cx('play')} onClick={handleClickPlay}>
+          <i className={cx('icon_play')}></i>
+        </button>
+      </div>
+    </div >
   );
 };
 
