55e2d5cd80c06f8f99e306ed429393db47b9ac38
DoQuangHuy <huydo@hivelab.com>
2022-11-17 11:16:10 +0700

    Fix Homepage.js

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,138 @@
+import classNames from 'classnames/bind';
+import { useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import videoApi from '../../api/videoApi';
+import { CupIcon, HelpIcon, PlateIcon } from '../../assets/Icons';
+import Modal from '../../components/Modal';
+import HelpModal from '../components/HelpModal';
+import MissionModal from '../components/MissionModal';
+import PushModal from '../components/PushModal';
+import SpinModal from '../components/SpinModal';
+import VideoSection from '../components/Video/VideoSection';
+import styles from './HomePage.module.scss';
+
+let cx = classNames.bind(styles);
+
+const HomePage = () => {
+  const { t } = useTranslation();
+
+  const [openHelp, setOpenHelp] = useState(false)
+  const [openSpin, setOpenSpin] = useState(false)
+  const [openPush, setOpenPush] = useState(false)
+  const [openMission, setOpenMission] = useState(false)
+  const [video, setVideo] = useState([])
+
+  const handleOpenHelpModal = () => {
+    setOpenHelp(true)
+  }
+  const handleCloseHelpModal = () => {
+    setOpenHelp(false)
+  }
+  const handleOpenSpinModal = () => {
+    setOpenSpin(true)
+  }
+  const handleCloseSpinModal = () => {
+    setOpenSpin(false)
+  }
+  const handleOpenPushModal = () => {
+    setOpenPush(true)
+  }
+  const handleClosePushModal = () => {
+    setOpenPush(false)
+  }
+
+  const handleOpenMissionModal = () => {
+    setOpenMission(true)
+  }
+  const handleCloseMisionModal = () => {
+    setOpenMission(false)
+  }
+
+  // get add video
+  useEffect(() => {
+    
+    videoApi.getCategoryList()
+      .then((reponse) => {
+        return reponse.data[0].id
+      })
+      .then((id) => {
+        return videoApi.getCategoryItem(id)
+      })
+      .then((reponse) => {
+        const list = reponse.data.map((item) => {
+          return {
+            link: item.videoDescription,
+            id: item.id,
+          }
+        })
+        setVideo(list);
+      })
+  }, [])
+
+  const countReward = useRef(null);
+
+  const [num, setNum] = useState(10000000000000);
+  useEffect(() => {
+    countReward.current = setInterval(() => {
+      setNum((countdown) => countdown + 100);
+    }, 1);
+
+    num > 99999999999999 && clearTimeout(countReward.current);
+
+    return () => {
+      clearTimeout(countReward.current);
+    }
+  }, [num])
+  
+  return (
+    <>
+      <div className={cx('wrapper')}>
+        {video.length > 1 && <VideoSection video={video} />}
+        <div className={cx('group')}>
+          <button type='button' className={cx('btn', 'get_spin')} onClick={handleOpenSpinModal}>{t("GET_MORE")}</button>
+          <button type='button' className={cx('btn', 'push')} onClick={handleOpenPushModal} >{t("PUSH_ADS")}</button>
+          <button type='button' className={cx('btn', 'add')} onClick={handleOpenMissionModal} >
+            +1000
+            <PlateIcon className={cx('icon_plate')} />
+          </button>
+        </div>
+        <div className={cx('reward')}>
+          <CupIcon className={cx('icon_cup')} />
+          <span className={cx('num')}>
+            {num} 
+          </span>
+            USD
+          <button className={cx('btn_help')} onClick={handleOpenHelpModal}>
+            <HelpIcon className={cx('icon_help')} />
+          </button>
+        </div>
+      </div>
+      {openHelp &&
+        <Modal onChange={handleCloseHelpModal} >
+          <HelpModal />
+        </Modal>
+      }
+
+      {openSpin &&
+        <Modal onChange={handleCloseSpinModal} >
+          <SpinModal />
+        </Modal>
+      }
+
+      {openPush &&
+        <Modal onChange={handleClosePushModal} >
+          <PushModal />
+        </Modal>
+      }
+
+      {openMission &&
+        <Modal onChange={handleCloseMisionModal} >
+          <MissionModal />
+        </Modal>
+      }
+    </>
+  )
+
+};
+
+export default HomePage;
